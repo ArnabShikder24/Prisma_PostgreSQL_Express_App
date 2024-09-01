a70896dffa07c8547f014b655c475335b6e465e0
ArnabShikder24 <arnabshikder2001@gmail.com>
2024-09-01 22:02:43 +0600

    migrate and generate

--- index.ts
+++ index.ts
@@ -4,16 +4,17 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 async function main() {
-  const getAllUser = await prisma.user.findMany();
-  console.log(getAllUser);
+  // const getAllUser = await prisma.user.findMany();
+  // console.log(getAllUser);
 
-  // const createUser = await prisma.user.create({
-  //   data: {
-  //     email: "arnab1@gmail.com",
-  //     name: "Arnab1"
-  //   }
-  // });
-  // console.log(createUser);
+  const createUser = await prisma.user.create({
+    data: {
+      email: "arnab2@gmail.com",
+      name: "Arnab2",
+      age: 23
+    }
+  });
+  console.log(createUser);
 }
 
 main();
